6c72f8b56dde786cb794cff51acb2a189962daa1
Rafael Santos <rafaels@gmail.com>
2026-01-06 21:38:52 -0500

    Finished adding the camera and sending rays into the scene, but not returning the right color gradient yet

--- main.cc
+++ main.cc
@@ -6,7 +6,9 @@
 #include "ray.h"
 
 color ray_color(const ray& r){
-	return color(0,0,0);
+	vec3 unitDirection = unit_vector(r.direction());
+	auto a = 0.5 * (unitDirection.y() + 1.0);
+	return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
 
 int main()
@@ -16,8 +18,10 @@ int main()
 	double focalLength = 1.0;
 	double viewportHeight = 2.0, viewportWidth;
 	point3 cameraCenter = point3(0, 0, 0); 
+	vec3 viewportU, viewportV, pixelDeltaU, pixelDeltaV;
+	vec3 viewportUpperLeft, pixel00Loc;
 	int imageWidth = 400, imageHeight;
-	color pixel_color;
+	color pixelColor;
 	int i, j;
 
 	// Calculate the image height and ensure that it is at least 1
@@ -26,7 +30,18 @@ int main()
 
 	// Camera
 	viewportWidth = viewportHeight * (double(imageWidth)/imageHeight);
-	
+
+	// Calculate the vectors across the horizontal and down the vertical viewport edges
+	viewportU = vec3(viewportWidth, 0, 0);
+	viewportV = vec3(viewportHeight, 0, 0);
+
+	// Calculate the horizontal and vertical delta vectors from pixel to pixel
+	pixelDeltaU = viewportU / imageWidth;
+	pixelDeltaV = viewportV / imageHeight;
+
+	// Calculate the loction of the upper left pixel
+	viewportUpperLeft = cameraCenter - vec3(0, 0, focalLength) - viewportU/2 - viewportV/2;
+	pixel00Loc = viewportUpperLeft + 0.5 * (pixelDeltaU + pixelDeltaV);
 
 	// Render
 	std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
@@ -34,8 +49,12 @@ int main()
 	for (j = 0; j < imageHeight; j++){
 		std::clog << "\rScanlines remaining: " << (imageHeight - j) << ' ' << std::flush;
 		for(i = 0; i < imageWidth; i ++){
-			pixel_color = color(double(i)/(imageWidth - 1), double(j)/(imageHeight - 1), 0);
-			write_color(std::cout, pixel_color);
+			auto pixelCenter = pixel00Loc + (i * pixelDeltaU) + (j * pixelDeltaV);
+			auto rayDirection = pixelCenter - cameraCenter;
+			ray r(cameraCenter, rayDirection);
+
+			pixelColor = ray_color(r);
+			write_color(std::cout, pixelColor);
 		}
 	}
 
